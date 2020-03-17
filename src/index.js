c603767c61afef9fba4b43eeb66644656aded7ba
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-03-17 22:00:03 +0300

    add solution task
    add solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+    if((typeof array==='undefined')||(array.length == 0)){
+     return 0;
+    } else {
+        let mn = array[0];
+        for (let a of array)
+          if (mn > a) mn = a;
+        return mn;
+      }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if((typeof array==='undefined')||(array.length == 0)){
+    return 0;
+   } else {
+        let mx = array[0];
+        for (let a of array)
+         if (mx < a) mx = a;
+      return mx;
+   }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if((typeof array==='undefined')||(array.length == 0)){
+    return 0;
+   } else {
+        let sum = 0;
+        for (let a of array) {
+          sum += a;
+      } 
+      sum = sum / array.length;
+      return sum.toFixed(2);
+    }
 }
